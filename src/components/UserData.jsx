5ea0fa7679c4fb1b7c303673ef4b7041a32965af
Germel Marata <germeljohnmarata@gmail.com>
2025-03-13 17:59:53 +0800

    modified user data for token expiration

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -9,7 +9,7 @@ const withUserData = (WrappedComponent) => {
     const [modalTitle, setModalTitle] = useState('');
     const [modalContent, setModalContent] = useState('');
 
-    const baseUrl = 'https://gold-blue-backend-zk1834563cke-84ddfc10b917.herokuapp.com';
+    const baseUrl = 'https://capital-trading-4fa9bbae4d39.herokuapp.com';
 
     const tokenLoggedOut = async () => {
       try {
